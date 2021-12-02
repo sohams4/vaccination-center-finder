af15cd0bbafa5a9953a986c50e4639270e74b0e3
Soham Shendre <sohamshendre04@gmail.com>
2021-12-02 21:07:08 +0530

    remove findbydistrict

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import './App.css';
 
 import FindByPincode from './components/FindByPincode';
@@ -6,32 +6,33 @@ import axios from 'axios';
 
 const BASE_URL = 'https://cdn-api.co-vin.in/api';
 const  PINCODE_ENDPOINT = '/v2/appointment/sessions/public/findByPin';
-const  DISTRICT_ENDPOINT = "/v2/appointment/sessions/public/findByDistrict";
+// const  DISTRICT_ENDPOINT = "/v2/appointment/sessions/public/findByDistrict";
 
 const App=() =>{
+  const [results, setResults] = React.useState([]);
   const handleSubmit = (event,type) => {
     event.preventDefault();
     const { target } = event;
     const { pincode } = target;
     
     if (type==="pincode") {
-      axios.get(BASE_URL + PINCODE_ENDPOINT + "?pincode=" + pincode.value +"&date=30-11-2021" ,{
+      axios.get(BASE_URL + PINCODE_ENDPOINT + "?pincode=" + pincode.value +"&date=1-12-2021" ,{
         headers: {
           'accept-language': 'en-IN',
           
         }
       }).then(response => {
-        console.log(response);
+       const { data: { sessions } } = response;
+    
+       setResults( sessions);
       }) 
     }
-    
   }
   return (
     <main>
       <h1>Find Vaccination Center Near You</h1>
-      <button>Find by district </button>
-      <button>Find by pincode </button>
       <FindByPincode handleSubmit={handleSubmit}/>
+      {JSON.stringify(results)}  
     </main>
   );
 }
